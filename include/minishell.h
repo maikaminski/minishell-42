81895698968992f4a8a92183d342c3397c9ce84d
Maira Kaminski <mahkaminski@gmail.com>
2025-06-02 15:17:38 -0300

    comentários com sugestões de structs

---
+++ include/minishell.h
@@ -0,0 +1,20 @@
+// struct de comandos
+
+//struct de redirecionamentos
+
+// struct de comandos
+
+// struct variáveis de ambiente
+
+// struct geral
+
+// possíveis tipos
+
+/*
+        #define WORD        1   // Palavra normal (comando ou argumento)
+        #define PIPE        2   // Pipe '|'
+        #define REDIR_IN    3   // Redirecionamento de entrada '<'
+        #define REDIR_OUT   4   // Redirecionamento de saída '>'
+        #define APPEND      5   // Append '>>'
+        #define HEREDOC     6   // Heredoc '<<'
+*/
