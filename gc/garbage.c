3c631502db278a4a0c16fba0f99a1cf2a39d5602
Maira Kaminski <mahkaminski@gmail.com>
2025-06-17 16:02:16 -0300

    .

--- gc/garbage.c
+++ gc/garbage.c
@@ -1,8 +1,8 @@
-#include "garbage_collector.h"
+#include "../include/garbage_collector.h"
 
-void	gc_malloc(size_t size, t_garbage **gc)
+void	*gc_malloc(size_t size, t_garbage **gc)
 {
-	void	*node;
+	void		*node;
 	t_garbage	*gc_node;
 
 	node = malloc(size);
@@ -22,10 +22,65 @@ void	gc_malloc(size_t size, t_garbage **gc)
 
 int	gc_add_ptr(void *ptr, t_garbage **gc)
 {
+	t_garbage	*new_node;
 
+	new_node = malloc(sizeof(t_garbage));
+	if (!new_node)
+		return (1);
+	new_node->ptr = ptr;
+	new_node->next = *gc;
+	*gc = new_node;
+	return (0);
 }
 
 void	gc_free_all(t_garbage **gc)
 {
+	t_garbage	*temp;
 
+	while (*gc)
+	{
+		temp = *gc;
+		free(temp->ptr);
+		*gc = temp->next;
+		free(temp);
+	}
 }
+// int main(void)
+// {
+// 	t_garbage *gc = NULL; // Começa vazio
+
+// 	// Aloca uma string com gc_malloc
+// 	char *nome = gc_malloc(100, &gc);
+// 	if (!nome)
+// 	{
+// 		printf("Erro ao alocar memória\n");
+// 		return (1);
+// 	}
+// 	strcpy(nome, "Maíra Rainha do Minishell");
+// 	printf("Nome alocado: %s\n", nome);
+
+// 	// Aloca um vetor de inteiros também
+// 	int *numeros = gc_malloc(sizeof(int) * 5, &gc);
+// 	if (!numeros)
+// 	{
+// 		printf("Erro ao alocar números\n");
+// 		gc_free_all(&gc);
+// 		return (1);
+// 	}
+// 	for (int i = 0; i < 5; i++)
+// 		numeros[i] = i * 10;
+
+// 	printf("Números: ");
+// 	for (int i = 0; i < 5; i++)
+// 		printf("%d ", numeros[i]);
+// 	printf("\n");
+
+// 	// Agora libera tudo com uma única função
+// 	gc_free_all(&gc);
+
+// 	// Verifica se limpou
+// 	if (gc == NULL)
+// 		printf("Memória limpa com sucesso!\n");
+
+// 	return (0);
+// }
