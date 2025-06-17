f58036e55cbb28e45c632e0cc40dc2745f527372
Maira Kaminski <mahkaminski@gmail.com>
2025-06-17 15:34:07 -0300

    feature garbage collector: implementação das funções gc_add_ptr e gc_free_all

--- include/garbage_collector.h
+++ include/garbage_collector.h
@@ -6,8 +6,12 @@
 // garbage collector
 typedef struct s_garbage
 {
-	void	*ptr;
-	struct s_garbage 	*next;
-} t_garbage;
+	void				*ptr;
+	struct s_garbage	*next;
+}	t_garbage;
+
+void	gc_malloc(size_t size, t_garbage **gc);
+int		gc_add_ptr(void *ptr, t_garbage **gc);
+void	gc_free_all(t_garbage **gc);
 
 #endif
