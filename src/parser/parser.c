b79e01e256e8e9d437e61d8c271385bd9d801438
Sabrina Santos De Lima <sabsanto@c3r2p3.42sp.org.br>
2025-07-21 21:51:00 -0300

    implementar parsing de aspas duplas com expansao

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,63 +6,83 @@
 /*   By: sabsanto <sabsanto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 20:04:28 by sabsanto          #+#    #+#             */
-/*   Updated: 2025/07/10 14:36:47 by sabsanto         ###   ########.fr       */
+/*   Updated: 2025/07/21 21:23:01 by sabsanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	tokenize(char *input)
+static void	process_operator_token(char *input, int *i)
 {
-	int	i;
-	int	len;
-	int	tok_len;
-	int	start;
+	if ((input[*i] == '>' || input[*i] == '<') && input[*i + 1] == input[*i])
+	{
+		printf("Token: %c%c\n", input[*i], input[*i + 1]);
+		*i += 2;
+	}
+	else
+	{
+		printf("Token: %c\n", input[*i]);
+		(*i)++;
+	}
+}
+
+static void	process_quoted_token(char *input, int *i, t_minishell *mini)
+{
 	char	*token;
 
+	if (input[*i] == '\'')
+		token = handle_single_quotes(input, i);
+	else if (input[*i] == '"')
+		token = handle_double_quotes(input, i, mini);
+	else
+		token = NULL;
+	if (!token)
+		return ;
+	printf("Token: %s\n", token);
+	free(token);
+}
+
+static void	process_word_token(char *input, int *i, int len)
+{
+	int		start;
+	int		tok_len;
+	char	*token;
+
+	start = *i;
+	while (*i < len && !is_space(input[*i]) && !is_operator(input[*i])
+		&& input[*i] != '\'' && input[*i] != '"')
+		(*i)++;
+	tok_len = *i - start;
+	token = malloc(tok_len + 1);
+	if (!token)
+		return ;
+	ft_strlcpy(token, &input[start], tok_len + 1);
+	printf("Token: %s\n", token);
+	free(token);
+}
+
+void	tokenize(char *input)
+{
+	t_minishell	mini;
+	int			i;
+	int			len;
+
+	if (!input)
+		return ;
+	mini = (t_minishell){0};
+	init_env_list(&mini, __environ);
 	i = 0;
 	len = ft_strlen(input);
 	while (i < len)
 	{
 		if (is_space(input[i]))
-		{
 			i++;
-			continue ;
-		}
-		if (is_operator(input[i]))
-		{
-			if ((input[i] == '>' || input[i] == '<') && input[i + 1] == input[i])
-			{
-				printf("Token: %c%c\n", input[i], input[i + 1]);
-				i += 2;
-			}
-			else
-			{
-				printf("Token: %c\n", input[i]);
-				i++;
-			}
-		}
+		else if (is_operator(input[i]))
+			process_operator_token(input, &i);
 		else if (input[i] == '\'' || input[i] == '"')
-		{
-			token = extract_quoted_token(input, &i);
-			if (!token)
-				return ;
-			printf("Token: %s\n", token);
-			free(token);
-		}
+			process_quoted_token(input, &i, &mini);
 		else
-		{
-			start = i;
-			while (i < len && !is_space(input[i]) && !is_operator(input[i])
-				&& input[i] != '\'' && input[i] != '"')
-				i++;
-			tok_len = i - start;
-			token = malloc(tok_len + 1);
-			if (!token)
-				return ;
-			ft_strlcpy(token, &input[start], tok_len + 1);
-			printf("Token: %s\n", token);
-			free(token);
-		}
+			process_word_token(input, &i, len);
 	}
+	gc_free_all(&mini.gc);
 }
--- src/parser/quote_handlers.c
+++ src/parser/quote_handlers.c
@@ -1,30 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parser_utils.c                                     :+:      :+:    :+:   */
+/*   quote_handlers.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sabsanto <sabsanto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/10 14:34:21 by sabsanto          #+#    #+#             */
-/*   Updated: 2025/07/21 21:22:41 by sabsanto         ###   ########.fr       */
+/*   Created: 2025/07/21 21:21:58 by sabsanto          #+#    #+#             */
+/*   Updated: 2025/07/21 21:23:21 by sabsanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*extract_quoted_token(char *input, int *i)
+char	*handle_single_quotes(char *input, int *i)
 {
 	int		start;
 	int		len;
-	char	quote_type;
 	char	*token;
 
-	quote_type = input[*i];
 	(*i)++;
 	start = *i;
-	while (input[*i] && input[*i] != quote_type)
+	while (input[*i] && input[*i] != '\'')
 		(*i)++;
-	if (input[*i] != quote_type)
+	if (input[*i] != '\'')
 	{
 		write(2, "minishell: syntax error: unclosed quotes\n", 42);
 		return (NULL);
@@ -34,7 +32,36 @@ char	*extract_quoted_token(char *input, int *i)
 	if (!token)
 		return (NULL);
 	ft_strlcpy(token, &input[start], len + 1);
-	if (input[*i] == quote_type)
-		(*i)++;
+	(*i)++;
 	return (token);
+}
+
+char	*handle_double_quotes(char *input, int *i, t_minishell *mini)
+{
+	int		start;
+	int		len;
+	char	*raw_token;
+	char	*expanded_token;
+
+	(*i)++;
+	start = *i;
+	while (input[*i] && input[*i] != '"')
+	{
+		if (input[*i] == '\\' && input[*i + 1] == '"')
+			*i += 2;
+		else
+			(*i)++;
+	}
+	if (input[*i] != '"')
+	{
+		write(2, "minishell: syntax error: unclosed quotes\n", 42);
+		return (NULL);
+	}
+	len = *i - start;
+	raw_token = extract_raw_content(input, start, len, &mini->gc);
+	if (!raw_token)
+		return (NULL);
+	expanded_token = expand_variables(raw_token, mini);
+	(*i)++;
+	return (expanded_token);
 }
